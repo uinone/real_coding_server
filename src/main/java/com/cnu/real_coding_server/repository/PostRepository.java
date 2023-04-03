09b4815e0bf746b38ab8cfcf5be7cceaf4254341
uinone <0911aza@naver.com>
2023-04-03 17:54:25 +0900

    [add] Post repository 완료

--- src/main/java/com/cnu/real_coding_server/repository/PostRepository.java
+++ src/main/java/com/cnu/real_coding_server/repository/PostRepository.java
@@ -4,10 +4,13 @@ import com.cnu.real_coding_server.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
-//    Post save(Post post);
-//    List<Post> findAll();
-//    Optional<Post> findById(Integer postId);
-//    void delete(Post post);
+    Post save(Post post);
+    List<Post> findAll();
+    Optional<Post> findById(Integer postId);
+    void delete(Post post);
 }
